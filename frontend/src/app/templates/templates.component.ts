c8ec0bbd8d095140b7dc2904df536068e252e6e6
gopipofficial <pgopirmd@gmail.com>
2020-12-07 21:17:25 +0530

    templates component added

---
+++ frontend/src/app/templates/templates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templates',
+  templateUrl: './templates.component.html',
+  styleUrls: ['./templates.component.sass']
+})
+export class TemplatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
