e0c47d4cd4732187e343b96f55ba80ae5cc5fb94
gopipofficial <pgopirmd@gmail.com>
2021-01-17 10:04:46 +0530

    few changes

--- backend/helpers/dbhelper.js
+++ backend/helpers/dbhelper.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
-    host: process.env.HOST, 
-    dialect: 'mysql',
+    host: process.env.HOST,
+    dialect: 'mysql', 
+    maxConcurrentQueries: 100,
     define: {
         freezeTableName: true,
-        timestamps: false
+        timestamps: false 
     }
 });
 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseurl: 'http://localhost:8000'
+  baseurl: 'http://localhost:3306'
 };
 
 /*
